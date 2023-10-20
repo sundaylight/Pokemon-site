603a0f9a644c77054af842445d55615b8e436f1f
Sundaylight <cintre@gmail>
2023-10-20 14:44:30 +0200

    pokemon-site

---
+++ homepage.js
@@ -0,0 +1,12 @@
+
+function changeimg1 () {
+    console.log ('toto')
+
+let poke1 = document.getElementById("poke1");
+
+ let imagevide1 = document.getElementById("imagevide1") 
+    console.log (imagevide1)
+    imagevide1.src= "pikachu.png";
+ }
+
+
